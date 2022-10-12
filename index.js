e0bd6e9e92dce1dcf5fcc7173f76563b0597dc02
shinar3@gmail.com <shenarfichman@Siddeeqahs-MacBook-Pro.local>
2022-10-12 19:26:29 -0400

    commit tree ba1eb99d93b7f77ddbb7b45044d836701e035786

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function toupperCaseCustomerName() {
+
+    console.log(customerName. toUpperCase)
+
+   customerName()
+
+
+}
+
+
+
+
+    
+
+
+
